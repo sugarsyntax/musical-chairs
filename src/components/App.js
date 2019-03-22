6c6320e6bd737fe24e65b8cd86a9050ab5d0f516
Jacquelline Beltran <jacquelline500@gmail.com>
2019-03-22 02:48:54 -0700

    Set up project, install styled components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>At the station bitches</h1>
+      </div>
+    );
+  }
+}
