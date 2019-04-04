256f1e0378ce5275210bd4bb02946914dcae4fcf
Jacquelline Beltran <jacquelline500@gmail.com>
2019-04-03 17:02:40 -0700

    Fix webpack config, project ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const H1 = styled.h1`
+  color: #27aebd;
+  text-align: center;
+`;
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>At the station bitches</h1>
+        <H1>At the station bitches</H1>
       </div>
     );
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("At the station bitches");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
